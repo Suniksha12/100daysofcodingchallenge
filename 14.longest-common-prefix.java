0a71ae1ccb1875ad1426d2db2e1d1e8eac8ab693
Suniksha <sunikshabenpatel@gmail.com>
2023-09-25 11:20:03 +0530

    added

---
+++ 14.longest-common-prefix.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+         if(strs.length==0) return "";
+         String prefix = strs[0];
+         for (int i = 1; i<strs.length;i++)
+         while(strs[i].indexOf(prefix)!=0){
+             prefix = prefix.substring(0,prefix.length()-1);
+         }
+         return prefix;
+
+    }
+}
+// @lc code=end
+
