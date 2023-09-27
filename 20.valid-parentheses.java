4bbda68d7e3dc0b7a7a0f9dd60ac876ce8021a7b
Suniksha <sunikshabenpatel@gmail.com>
2023-09-27 13:51:41 +0530

    added

---
+++ 20.valid-parentheses.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        //creating an empty stack for proper maintaineance of order
+     Stack <Character> stack = new Stack<>();
+     
+     for(char x : s.toCharArray()){ //loop through each character in the string
+        //performing the instructions as per given in the question looking for opening and closing brackets.
+         if(x=='('){
+             stack.push(')');
+         } else if(x=='['){
+             stack.push(']');
+         } else if (x=='{'){
+             stack.push('}');
+         } else if(stack.isEmpty() || stack.pop()!=x){
+             //if the stack is found to be empty or the string is not matching to opening or closing bracket then the string is not valid hence returning false.
+             return false;
+         }
+     }
+     return stack.isEmpty();
+    }
+}
+// @lc code=end
+
