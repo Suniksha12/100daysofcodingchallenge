74626dae768cdbd85955c9173da450ba5ff4db4e
Suniksha <sunikshabenpatel@gmail.com>
2023-10-13 21:29:27 +0530

    added

---
+++ 35.search-insert-position.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+         int start = 0;
+        int end = nums.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+
+        return start;
+    }
+}
+// @lc code=end
+
