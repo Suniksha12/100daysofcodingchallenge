c79671aacf10b2eb26c407ce0ce7348cb313de39
Suniksha <sunikshabenpatel@gmail.com>
2023-10-20 22:13:16 +0530

    added

---
+++ 290.word-pattern.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=290 lang=java
+ *
+ * [290] Word Pattern
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        if(pattern.length()!=arr.length)
+        return false;
+        HashMap<Character,String> map = new HashMap<Character,String>();
+        for(int i=0;i<arr.length;i++){
+            char c = pattern.charAt(i);
+            String str = arr[i];
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(str))
+                return false;
+            }
+            else {
+                if(map.containsValue(str))
+                return false;
+                map.put(c,str);
+            }
+    }
+    return true;
+    }
+}
+// @lc code=end
+
