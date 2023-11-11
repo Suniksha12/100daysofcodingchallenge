61be6c2f73d9566a544e8877a48f53f1cc22a037
Suniksha <sunikshabenpatel@gmail.com>
2023-11-11 21:12:08 +0530

    changes

---
+++ 6.zigzag-conversion.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=6 lang=java
+ *
+ * [6] Zigzag Conversion
+ */
+
+// @lc code=start
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        int n = s.length();
+        int diff = 2 * (numRows - 1);
+        int diagonalDiff = diff;
+        int secondIndex;
+        int index;
+        for (int i = 0; i < numRows; i++) {
+            index = i;
+
+            while (index < n) {
+                answer.append(s.charAt(index));
+                if (i != 0 && i != numRows - 1) {
+                    diagonalDiff = diff-2*i;
+                    secondIndex = index + diagonalDiff;
+                    
+                    if (secondIndex < n) {
+                        answer.append(s.charAt(secondIndex));
+                    }
+                }
+                index += diff;
+            }
+        }
+        
+        return answer.toString();
+    }
+}
+// @lc code=end
+
