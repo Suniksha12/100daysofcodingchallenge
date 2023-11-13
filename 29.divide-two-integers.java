e6bb9ca7e59489db647578997b51f1b96edba205
Suniksha <sunikshabenpatel@gmail.com>
2023-11-13 21:23:44 +0530

    changes

---
+++ 29.divide-two-integers.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=29 lang=java
+ *
+ * [29] Divide Two Integers
+ */
+
+// @lc code=start
+class Solution {
+    public int divide(int dividend, int divisor) {
+        boolean isNegative = (dividend < 0 && divisor >0) || (dividend > 0 && divisor < 0) ? true : false;
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        long result =0;
+        while(absDividend >= absDivisor) {
+            long tmp = absDivisor, count = 1;
+            while(tmp <= absDividend) {
+                tmp <<= 1;
+                count <<= 1;
+            }
+            result += count >> 1;
+            absDividend -= tmp>>1;
+        }
+        return isNegative ? (int) ~result + 1: result >Integer.MAX_VALUE? Integer.MAX_VALUE : (int) result;
+    }
+}
+// @lc code=end
+
