d73d4399c4e1c9bd07140ed926904cd0379aa8a5
Suniksha <sunikshabenpatel@gmail.com>
2023-11-23 23:35:19 +0530

    changes

---
+++ 50.pow-x-n.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+public class Solution {
+    public double solve(double x, long n) {
+        if (n == 0) {
+            return 1; // power of 0 is 1
+        }
+        double temp = solve(x, n / 2);
+        temp = temp * temp;
+
+        if (n % 2 == 0) { // if even, return just without doing anything
+            return temp;
+        } else {
+            return temp * x; // if odd, return by multiplying once more with the given number
+        }
+    }
+
+    public double myPow(double x, int n) {
+        long longN = n; // Convert n to a long to handle Integer.MIN_VALUE
+        double ans = solve(x, Math.abs(longN));
+
+        if (longN < 0) {
+            return 1 / ans;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
